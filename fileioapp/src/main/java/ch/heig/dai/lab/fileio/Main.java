71e23a565cb38086c48ef7eb51870363e1df34e5
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 12:21:49 +0200

    Replaced space indents with tabs

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -5,39 +5,43 @@ import java.io.File;
 import ch.heig.dai.lab.fileio.chomusukeworks.*;
 
 public class Main {
-    private static final String newName = "Artur";
+	private static final String newName = "Artur";
 
-    /**
-     * Main method to transform files in a folder.
-     * Create the necessary objects (FileExplorer, EncodingSelector, FileReaderWriter, Transformer).
-     * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
-     * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
-     * FileReaderWriter.
-     * <p>
-     * Result files are written in the same folder as the input files, and encoded with UTF8.
-     * <p>
-     * File name of the result file:
-     * an input file "myfile.utf16le" will be written as "myfile.utf16le.processed",
-     * i.e., with a suffixe ".processed".
-     */
-    public static void main(String[] args) {
-        // Read command line arguments
-        if (args.length != 2 || !new File(args[0]).isDirectory()) {
-            System.out.println("You need to provide two command line arguments: an existing folder and the number of words per line.");
-            System.exit(1);
-        }
-        String folder = args[0];
-        int wordsPerLine = Integer.parseInt(args[1]);
-        System.out.println("Application started, reading folder " + folder + "...");
-        // TODO: implement the main method here
+	/**
+	 * Main method to transform files in a folder.
+	 * Create the necessary objects (FileExplorer, EncodingSelector, FileReaderWriter, Transformer).
+	 * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
+	 * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
+	 * FileReaderWriter.
+	 * <p>
+	 * Result files are written in the same folder as the input files, and encoded with UTF8.
+	 * <p>
+	 * File name of the result file:
+	 * an input file "myfile.utf16le" will be written as "myfile.utf16le.processed",
+	 * i.e., with a suffixe ".processed".
+	 */
+	public static void main(String[] args) {
+		// Read command line arguments
+		if (args.length != 2 || !new File(args[0]).isDirectory()) {
+			System.out.println("You need to provide two command line arguments: an existing folder and the number of words per line.");
+			System.exit(1);
+		}
+		String folder = args[0];
+		int wordsPerLine = Integer.parseInt(args[1]);
+		System.out.println("Application started, reading folder " + folder + "...");
 
-        while (true) {
-            try {
-                // TODO: loop over all files
+		FileExplorer fileExplorer = new FileExplorer(folder);
+		EncodingSelector encodingSelector = new EncodingSelector();
+		FileReaderWriter fileReaderWriter = new FileReaderWriter();
+		Transformer transformer = new Transformer(newName, wordsPerLine);
 
-            } catch (Exception e) {
-                System.out.println("Exception: " + e);
-            }
-        }
-    }
+		while (true) {
+			try {
+				// TODO: loop over all files
+
+			} catch (Exception e) {
+				System.out.println("Exception: " + e);
+			}
+		}
+	}
 }
