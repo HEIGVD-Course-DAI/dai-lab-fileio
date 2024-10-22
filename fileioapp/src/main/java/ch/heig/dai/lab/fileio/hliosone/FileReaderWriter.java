72877161f71b88f8ce1ec105fed85464729eba99
hliosone <sstelcher@protonmail.com>
2024-10-22 18:14:43 +0200

    FileReaderWriter done

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileReaderWriter.java
@@ -8,14 +8,32 @@ public class FileReaderWriter {
     /**
      * Read the content of a file with a given encoding.
      * @param file the file to read. 
-     * @param encoding
+     * @param encoding file encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+        if (file == null || encoding == null) {
+            return null;
+        }
+
+        try(
+                FileInputStream fileInputStream = new FileInputStream(file);
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, encoding);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+        ){
+            StringBuilder sbuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sbuilder.append(line).append(System.lineSeparator());
+            }
+            return sbuilder.toString();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e);
+            return null;
+        }
     }
 
     /**
@@ -29,6 +47,18 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, encoding);
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)
+                ){
+            bufferedWriter.write(content);
+            bufferedWriter.flush();
+            return true;
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e);
+            return false;
+        }
     }
 }
