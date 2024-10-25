1d0c4b0898e3acaf55392bcc8fad8b9c71084c74
Pandab <nadiacattin@bluewin.ch>
2024-10-25 17:54:56 +0200

    Main implementation

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,12 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
-
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import ch.heig.dai.lab.fileio.nadi3.*;
 
 public class Main {
-    // *** TODO: Change this to your own name ***
-    private static final String newName = "Edison";
+    private static final String newName = "Nadia Cattin";
 
     /**
      * Main method to transform files in a folder.
@@ -15,7 +14,7 @@ public class Main {
      * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
      * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
      * FileReaderWriter.
-     * 
+     *
      * Result files are written in the same folder as the input files, and encoded with UTF8.
      *
      * File name of the result file:
@@ -31,12 +30,30 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
-        // TODO: implement the main method here
-
+        FileExplorer explorer = new FileExplorer(folder);
+        EncodingSelector selector = new EncodingSelector();
+        FileReaderWriter readerWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
         while (true) {
             try {
-                // TODO: loop over all files
+                File file = explorer.getNewFile();
+                if (null == file) {
+                    break;
+                }
 
+                Charset encoding = selector.getEncoding(file);
+                String content = readerWriter.readFile(file, encoding);
+                String transformed = transformer.wrapAndNumberLines(
+                        transformer.capitalizeWords(
+                                transformer.replaceChuck(content)
+                        )
+                );
+                String newFileName = folder + File.separator + file.getName() + ".processed";
+                if (readerWriter.writeFile(new File(newFileName), transformed, StandardCharsets.UTF_8)) {
+                    System.out.println("Success for file: " + newFileName);
+                } else {
+                    System.out.println("Error for file: " + newFileName);
+                }
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
             }
