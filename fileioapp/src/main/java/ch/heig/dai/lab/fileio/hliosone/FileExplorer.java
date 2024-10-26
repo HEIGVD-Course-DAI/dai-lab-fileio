2987f86fc9596ccf498058baead147f1306ab653
hliosone <sstelcher@protonmail.com>
2024-10-27 00:19:39 +0200

    Removed changed from other feature

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileExplorer.java
@@ -26,19 +26,6 @@ public class FileExplorer {
      */
     public File getNewFile() {
         // TODO: implement the method body here
-        if (folder.isDirectory() && folder.exists()){
-            File[] files = folder.listFiles();
-
-            if (files != null){
-                for (File file : files){
-                    if (!knownFiles.contains(file)){
-                        knownFiles.add(file);
-                        return file;
-                    }
-                }
-            }
-        }
-
         return null;
     }
 }
