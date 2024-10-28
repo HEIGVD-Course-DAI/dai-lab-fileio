15abe720182d0d460dbf325dcec41adf3771a4b1
IlianTopalov <ilian.topalov@heig-vd.ch>
2024-10-28 10:31:21 +0100

    Undochanges on main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/IlianTopalov/Transformer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/IlianTopalov/Transformer.java
@@ -24,7 +24,7 @@ public class Transformer {
      */
     public String replaceChuck(String source) {
         // TODO: Implement the method body here.
-        return source.replaceAll("Chuck Norris", newName);
+        return "";
     }
 
     /**
@@ -34,16 +34,7 @@ public class Transformer {
      */
     public String capitalizeWords(String source) {
         // TODO: Implement the method body here.
-        String[] words = source.split(" ");
-        StringBuilder capitalized = new StringBuilder();
-
-        for (String word : words) {
-            capitalized.append(word.substring(0, 1).toUpperCase())
-                        .append(word.substring(1))
-                        .append(" ");
-        }
-        // Remove last appended space
-        return capitalized.toString().trim();
+        return "";
     }
 
     /**
@@ -55,29 +46,6 @@ public class Transformer {
     public String wrapAndNumberLines(String source) {
         // TODO: Implement the method body here.
         // Use the StringBuilder class to build the result string.
-        if (numWordsPerLine < 1) {
-            return source;
-        }
-
-        String[] words = source.split(" ");
-
-        int wordCounter = 0;
-        StringBuilder wrap = new StringBuilder();
-
-        for (String word : words) {
-            if (wordCounter >= numWordsPerLine) {
-                wrap.append("\n"); // Start a new line after reaching numWordsPerLine words
-                wordCounter = 0;
-            }
-
-            if (wordCounter > 0) { // Separate words by spaces while numWordsPerLine not reached
-                wrap.append(" ");
-            }
-
-            wrap.append(word); // Append word
-            ++wordCounter;
-        }
-
-        return wrap.toString();
+        return "";
     }
 }   
