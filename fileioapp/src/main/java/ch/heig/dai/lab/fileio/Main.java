083c0a921cbd0581e1e216866b40568db9c3c7b1
IlianTopalov <ilian.topalov@heig-vd.ch>
2024-10-28 15:48:57 +0100

    reverted to original main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,15 +1,13 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.IlianTopalov.*;
+import ch.heig.dai.lab.fileio.jehrensb.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "IlianTopalov";
+    private static final String newName = "Jean-Claude Van Damme";
 
     /**
      * Main method to transform files in a folder.
@@ -17,7 +15,7 @@ public class Main {
      * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
      * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
      * FileReaderWriter.
-     * 
+     *
      * Result files are written in the same folder as the input files, and encoded with UTF8.
      *
      * File name of the result file:
@@ -26,7 +24,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // Read command line arguments
-        System.out.println(Arrays.toString(args));
         if (args.length != 2 || !new File(args[0]).isDirectory()) {
             System.out.println("You need to provide two command line arguments: an existing folder and the number of words per line.");
             System.exit(1);
@@ -34,41 +31,11 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
+        // TODO: implement the main method here
 
-        FileExplorer fileExplorer = new FileExplorer(folder);
-        EncodingSelector encodingSelector = new EncodingSelector();
-        FileReaderWriter fileReaderWriter = new FileReaderWriter();
-        Transformer transformer = new Transformer(newName, wordsPerLine);
-
-        String suffix = ".processed";
-
-        // loop over all files
         while (true) {
             try {
-                File file = fileExplorer.getNewFile();
-                if (file == null) {
-                    break;
-                }
-
-                if (file.getName().endsWith(suffix)) {
-                    continue;
-                }
-
-                // Get file encoding
-                Charset encoding = encodingSelector.getEncoding(file);
-
-                // Get file content
-                String content = fileReaderWriter.readFile(file, encoding);
-
-                // Transform content
-                content = transformer.replaceChuck(content);
-                content = transformer.capitalizeWords(content);
-                content = transformer.wrapAndNumberLines(content);
-
-                // Output content
-                String outputFileName = file.getAbsolutePath() + suffix;
-                File outputFile = new File(outputFileName);
-                fileReaderWriter.writeFile(outputFile, content, encoding);
+                // TODO: loop over all files
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
