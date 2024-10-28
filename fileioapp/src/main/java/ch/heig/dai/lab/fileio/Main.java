244a166ca5d57c34a0bb38594ee23ba3f2cb45cc
Bénédicte Vernet <benedicte.vernet@arcanite.ch>
2024-10-28 13:29:13 +0100

    implements the Main class

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -2,12 +2,13 @@ package ch.heig.dai.lab.fileio;
 
 import java.io.File;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.bvernet.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+
+    private static final String newName = "Bénédicte Vernet";
 
     /**
      * Main method to transform files in a folder.
@@ -31,14 +32,39 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
-        // TODO: implement the main method here
+
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        EncodingSelector encoderSelector = new EncodingSelector();
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
 
         while (true) {
             try {
-                // TODO: loop over all files
+
+                File newFile = fileExplorer.getNewFile();
+                if (newFile == null) {
+                    break;
+                }
+
+                Charset fileCharset = encoderSelector.getEncoding(newFile);
+                if (fileCharset == null) {
+                    continue;
+                }
+
+                File outputFile = new File(newFile.getAbsolutePath() + ".processed");
+
+                String contentFile = fileReaderWriter.readFile(newFile, fileCharset);
+                
+                String newContentFile = transformer.replaceChuck(contentFile);
+                newContentFile = transformer.capitalizeWords(newContentFile);
+                newContentFile = transformer.wrapAndNumberLines(newContentFile);
+                newContentFile = newContentFile.replaceAll("\n\n", "");
+
+                fileReaderWriter.writeFile(outputFile, newContentFile, StandardCharsets.UTF_8);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
+                break;
             }
         }
     }
