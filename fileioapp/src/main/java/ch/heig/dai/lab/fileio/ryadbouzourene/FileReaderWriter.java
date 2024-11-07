fca5a33b69688d1445d408abdf8423ec797c7452
Juergen Ehrensberger <juergen.ehrensberger@gmail.com>
2024-11-07 15:09:50 +0100

    Manually add FileReaderWriter from ryadbouzourene

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/ryadbouzourene/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/ryadbouzourene/FileReaderWriter.java
@@ -12,21 +12,20 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
+        
+        StringBuilder content = new StringBuilder();
 
-        try (var reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))){
+        try (var reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                content.append(line).append(System.lineSeparator());
             }
-
-
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Exception: " + e);
-        };
-        return null;
+            return null; 
+        }
+
+        return content.toString(); // Retourne le contenu complet
     }
 
     /**
@@ -41,7 +40,7 @@ public class FileReaderWriter {
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
         try(var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));){
-            writer.write(content, 0, 0);
+            writer.write(content);
             writer.flush();
             return true ;
         }catch(IOException e){
